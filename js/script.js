2cbe639ad0871d0915140e12a9449882bc98a40a
rodrigo-cos <rodrigo.cos90@gmail.com>
2023-04-03 16:54:43 -0300

    se agrego el script

---
+++ js/script.js
@@ -0,0 +1,28 @@
+console.log("Hola mundo")
+const usuarios=[]
+
+const formularioRegistro=document.querySelector(".form")
+const contenedorHTML= document.querySelector("#contenedorUsuarios")
+const renderizarUsuarios= ()=>{
+    contenedorHTML.innerHTML=""
+  for(const usuario of usuarios)
+  {
+    contenedorHTML.innerHTML+=`
+    <div>
+    <h2>Nombre: ${usuario.nombre}</h2>
+    <p>Email: <strong>${usuario.email}</strong></p>
+    <p>Contraseña: <strong>${usuario.contrasena}</strong></p> 
+    </div>
+    `
+  }
+}
+
+formularioRegistro.addEventListener("submit",(event)=>{
+    event.preventDefault()
+    usuarios.push({
+        nombre: formularioRegistro.nombreCompleto.value,
+        email: formularioRegistro.correoElectronico.value,
+        contrasena:formularioRegistro.contrasena.value
+    })
+    renderizarUsuarios()
+})
